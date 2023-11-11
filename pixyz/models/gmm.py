aea211355e8e698e354f9f5945e92fbeb70648f7
Ito Masaki <ito.masaki@em.ci.ritsumei.ac.jp>
2023-11-11 15:59:22 +0900

    [Fix]

--- pixyz/models/gmm.py
+++ pixyz/models/gmm.py
@@ -73,17 +73,15 @@ class GMM(Model):
 
         # GMM学習
         for _ in range(self.epoch):
-            for x, _ in loader:
-                input_dict = {"x": x}
-                loss = self.train(input_dict)
+            # for x, _ in loader:
+            input_dict = {"x": torch.Tensor(data[0])}
+            loss = self.train(input_dict)
 
         # Passing the message
         Pdz = self.post.prob().eval({"x": torch.Tensor(data[0])}).detach().cpu().numpy() # P(z|d)
         Pdz = (Pdz / np.sum(Pdz, 0))
-        # mu = [self.p.distributions[idx].sample()["x"].detach().cpu().numpy() for idx in np.argmax(Pdz, 0)] # P(d|z)
         mu = [self.p.distributions[idx].loc.detach().cpu().numpy() for idx in np.argmax(Pdz, 0)] # P(d|z)
         
-        # self.__n += 1
 
         # メッセージの送信
         self.set_forward_msg(Pdz)
