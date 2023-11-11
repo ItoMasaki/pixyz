0152f44a7fd0290e096e3fd0d34e6d3d2b9b54a5
Ito Masaki <ito.masaki@em.ci.ritsumei.ac.jp>
2023-11-11 13:50:33 +0900

    [Fix] RuntimeError: The size of tensor a (24) must match the size of tensor b (31) at non-singleton dimension 0

--- pixyz/models/vae.py
+++ pixyz/models/vae.py
@@ -77,7 +77,8 @@ class VAE(Model):
 
         # If mu_prior is not calculated yet
         if mu_prior is None:
-            mu_prior = torch.zeros(self.kwargs["batch_size"], self.kwargs["latent_dim"])
+            # mu_prior = torch.zeros(self.kwargs["batch_size"], self.kwargs["latent_dim"])
+            mu_prior = torch.zeros(1., self.kwargs["latent_dim"])
         else:
             mu_prior = torch.Tensor(mu_prior)
         self.prior.loc = mu_prior
